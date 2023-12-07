f5a2d2bc47a4540236cc3de39c1591879ad00d0f
1099238610 <68647376+1099238610@users.noreply.github.com>
2023-12-07 13:20:18 +0800

    add spring security config

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -2,18 +2,38 @@ package org.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 /**
- * @author gzh
+ * @author 高子航
  */
 @Configuration
 public class SecurityConfig {
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        
+        // 设置权限, 除了 login 和 register 接口, 其他都需要身份验证
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/user/login", "/user/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+        
+        // 关闭 CSRF 保护
+        http.csrf(AbstractHttpConfigurer::disable);
+        
+        return http.build();
     }
 }
 
